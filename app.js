60f93b5616cb2872d092c8b54d05da7831ce7c39
carsin <carsin@users.noreply.github.com>
2017-10-14 11:05:12 -1000

    Chat room functionality to learn sockets

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get("/", function(req, res) {
 
 io.on("connection", function(socket) {
     console.log("A user has connected.");
+
+    socket.on("chat message", function(msg) {
+        io.emit('chat message', msg);
+    });
+
     socket.on("disconnect", function() {
         console.log("A user has disconnected.");
     });
