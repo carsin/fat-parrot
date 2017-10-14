a2543b3843da19ee5ceb566af3d41679eb01460f
carsin <carsin@users.noreply.github.com>
2017-10-14 10:59:09 -1000

    Implement socket.io

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const socketio = require("socket.io");
+const http = require("http").Server(app);
+const io = socketio(http);
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function() {
+io.on("connection", function(socket) {
+    console.log("A user has connected.");
+    socket.on("disconnect", function() {
+        console.log("A user has disconnected.");
+    });
+});
+
+http.listen(3000, function() {
     console.log("Server started on port 3000")
 });
