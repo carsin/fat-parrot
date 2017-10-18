06c0677c046d258b67f7a7fa6119163ec180e53e
carson <carsin@users.noreply.github.com>
2017-10-18 12:10:42 -1000

    js: comment

--- app.js
+++ app.js
@@ -11,35 +11,39 @@ const http = require("http").Server(app);
 const socketio = require("socket.io");
 const io = socketio(http);
 
-var clientCount = 0;
+var clientCount = 0; // count of users online
 
+// render index ejs page
 app.get("/", (req, res) => {
     res.render("pages/index");
 });
 
+// connect event
 io.sockets.on("connection", (socket) => {
     clientCount++;
     console.log("A user has connected. Total users: " + clientCount);
 
+    // this section is broken rn.
     socket.on("speak", (msg, user) => {
         say.speak(msg.substring(0, 200), "Alex", 1.0, (err) => {
             if (err) {
                 console.error(err);
             }
-
             console.log(user + " said " + msg)
         });
-        console.log(msg + " from " + user)
     });
-
+    
+    // usercount event
     socket.on("update usercount", () => {
         io.sockets.emit("update usercount", clientCount);
     });
 
+    // chat event
     socket.on("chat message", function(msg, user) {
         io.emit('chat message', user + ": " + msg);
     });
 
+    // disconnect event
     socket.on("disconnect", () => {
         clientCount--;
         io.sockets.emit("update usercount", clientCount);
@@ -47,6 +51,7 @@ io.sockets.on("connection", (socket) => {
     });
 });
 
+// start server
 http.listen(3000, () => {
     console.log("Server started on port 3000")
 });
